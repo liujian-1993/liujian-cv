ca88ae8f04f23b9106f9f5c89479b0799bf48c7b
Stephen <1137657002@qq.com>
2021-03-27 14:52:05 +0800

    重新排版

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -118,7 +118,53 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('skills')); // 指定图表的配置项和数据
 
+var option = {
+  tooltip: {},
+  radar: {
+    // shape: 'circle',
+    name: {
+      textStyle: {
+        color: '#fff',
+        backgroundColor: '#999',
+        borderRadius: 3,
+        padding: [3, 5]
+      }
+    },
+    indicator: [{
+      name: 'JavaScript',
+      max: 100
+    }, {
+      name: '静态页面',
+      max: 100
+    }, {
+      name: 'JQuery',
+      max: 100
+    }, {
+      name: 'TypeScript',
+      max: 100
+    }, {
+      name: 'Vue',
+      max: 100
+    }, {
+      name: '沟通能力',
+      max: 100
+    }]
+  },
+  series: [{
+    name: '预算 vs 开销（Budget vs spending）',
+    type: 'radar',
+    // areaStyle: {normal: {}},
+    data: [{
+      value: [70, 95, 90, 80, 80, 65],
+      name: '能力雷达'
+    }]
+  }]
+}; // 使用刚指定的配置项和数据显示图表。
+
+myChart.setOption(option);
 },{}],"../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62820" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52645" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
